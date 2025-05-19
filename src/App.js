f934c610328e0ad01144252a44909abbfcb204e3
tulbureandreita <tulbureandreit@gmail.com>
2025-05-19 19:58:30 +0300

    formatter test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Facial comparison app
-        </h1>
+        <h1>Facial comparison app</h1>
       </header>
     </div>
   );
